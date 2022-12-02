397b4a65c1e3690cae88c53ea93eb24968bdd530
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-02 09:25:50 -0800

    First round done

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -1,17 +1,83 @@
 # day one
-calorie_list = File.readlines 'calorie_list.rb' 
-def find_most_calories(list)
-    each_elf_backpack_arr = []
-    individual_elf_pack = 0
-    list.each do |calorie|
-        if calorie.length == 1
-            each_elf_backpack_arr.push(individual_elf_pack)
-            individual_elf_pack = 0
+# calorie_list = File.readlines 'calorie_list.rb' 
+# def find_most_calories(list)
+#     each_elf_backpack_arr = []
+#     individual_elf_pack = 0
+#     list.each do |calorie|
+#         if calorie.length == 1
+#             each_elf_backpack_arr.push(individual_elf_pack)
+#             individual_elf_pack = 0
+#         else
+#             individual_elf_pack += calorie.to_i
+#         end
+#     end
+#     max_value = each_elf_backpack_arr.max(3).reduce(:+)
+#     return max_value
+# end
+# puts find_most_calories(calorie_list)
+
+
+# day two 
+moves_list = File.readlines 'strategy_list.rb' 
+# Rock = X, Paper = Y, Scissors = Z     --- Rock = 1, Paper = 2, Scissors = 3
+# Rock = A, Paper = B, Scissors = C     --- Win = 6, Draw = 3, Loss = 0
+def calculate_score(list)
+    total_score = 0
+    list.each do |round|
+        splitted = round.split(" ")
+        opponent_move = splitted[0]
+        my_move = splitted[1]
+        round_moves = opponent_move + "-" + my_move
+        # first round logic
+        # case round_moves
+        # when "A-X"
+        #     total_score += 4 
+        # when "A-Y"
+        #     total_score += 8 
+        # when "A-Z"
+        #     total_score += 3 
+        # when "B-X"
+        #     total_score += 1 
+        # when "B-Y" 
+        #     total_score += 5 
+        # when "B-Z"
+        #     total_score += 9 
+        # when "C-X"
+        #     total_score += 7 
+        # when "C-Y"
+        #     total_score += 2 
+        # when "C-Z"
+        #     total_score += 6 
+        # else
+        #     invalid_input += 1
+        # end
+        #
+        #
+        
+        # second round logic
+        case round_moves
+        when "A-X"
+            total_score += 4 
+        when "A-Y"
+            total_score += 8 
+        when "A-Z"
+            total_score += 3 
+        when "B-X"
+            total_score += 1 
+        when "B-Y" 
+            total_score += 5 
+        when "B-Z"
+            total_score += 9 
+        when "C-X"
+            total_score += 7 
+        when "C-Y"
+            total_score += 2 
+        when "C-Z"
+            total_score += 6 
         else
-            individual_elf_pack += calorie.to_i
+            invalid_input += 1
         end
     end
-    max_value = each_elf_backpack_arr.max(3).reduce(:+)
-    return max_value
+    return total_score
 end
-puts find_most_calories(calorie_list)
+puts calculate_score(moves_list)
