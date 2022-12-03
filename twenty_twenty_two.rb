54326e05a9fee1f4406c3a7a1ecf49816d8d6304
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-03 07:43:14 -0800

    Day 3 Part One

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -17,61 +17,81 @@
 # puts find_most_calories(calorie_list)
 
 # day two 
-moves_list = File.readlines 'strategy_list.rb' 
-def calculate_score(list)
-    total_score = 0
-    list.each do |round|
-        splitted = round.split(" ")
-        round_moves = splitted[0] + "-" + splitted[1]
-        ### first round conditional logic 
-        # case round_moves
-        # when "A-X"
-        #     total_score += 4 
-        # when "A-Y"
-        #     total_score += 8 
-        # when "A-Z"
-        #     total_score += 3 
-        # when "B-X"
-        #     total_score += 1 
-        # when "B-Y" 
-        #     total_score += 5 
-        # when "B-Z"
-        #     total_score += 9 
-        # when "C-X"
-        #     total_score += 7 
-        # when "C-Y"
-        #     total_score += 2 
-        # when "C-Z"
-        #     total_score += 6 
-        # else
-        #     invalid_input += 1
-        # end
-        ### second round conditional logic
-        case round_moves
-        when "A-X"
-            total_score += 3 #
-        when "A-Y"
-            total_score += 4 #
-        when "A-Z"
-            total_score += 8 #
-        when "B-X"
-            total_score += 1 #
-        when "B-Y" 
-            total_score += 5 #
-        when "B-Z"
-            total_score += 9 #
-        when "C-X"
-            total_score += 2 #
-        when "C-Y"
-            total_score += 6 #
-        when "C-Z"
-            total_score += 7 #
-        else
-            puts "invalid move"
+# moves_list = File.readlines 'strategy_list.rb' 
+# def calculate_score(list)
+#     total_score = 0
+#     list.each do |round|
+#         splitted = round.split(" ")
+#         round_moves = splitted[0] + "-" + splitted[1]
+#         ### first round conditional logic 
+#         # case round_moves
+#         # when "A-X"
+#         #     total_score += 4 
+#         # when "A-Y"
+#         #     total_score += 8 
+#         # when "A-Z"
+#         #     total_score += 3 
+#         # when "B-X"
+#         #     total_score += 1 
+#         # when "B-Y" 
+#         #     total_score += 5 
+#         # when "B-Z"
+#         #     total_score += 9 
+#         # when "C-X"
+#         #     total_score += 7 
+#         # when "C-Y"
+#         #     total_score += 2 
+#         # when "C-Z"
+#         #     total_score += 6 
+#         # else
+#         #     invalid_input += 1
+#         # end
+#         ### second round conditional logic
+#         case round_moves
+#         when "A-X"
+#             total_score += 3 #
+#         when "A-Y"
+#             total_score += 4 #
+#         when "A-Z"
+#             total_score += 8 #
+#         when "B-X"
+#             total_score += 1 #
+#         when "B-Y" 
+#             total_score += 5 #
+#         when "B-Z"
+#             total_score += 9 #
+#         when "C-X"
+#             total_score += 2 #
+#         when "C-Y"
+#             total_score += 6 #
+#         when "C-Z"
+#             total_score += 7 #
+#         else
+#             puts "invalid move"
+#         end
+#     end
+#     return total_score
+# end
+# puts calculate_score(moves_list)
+
+# day 3 
+rucksack_list = File.readlines 'rucksack_list.rb'
+def find_priority_score(rucksack)
+    score = 0
+    rucksack.each do |sack|
+        first_half = sack[0, sack.size.to_f / 2]
+        second_half = sack[-(sack.size.to_f / 2) - 1, sack.size - 1]
+        common_letter = first_half.split("") & second_half.split("")
+        char_points = common_letter[0].ord
+        if char_points > 96 
+            score += char_points - 96
+        elsif char_points < 91
+            score += char_points - 38
         end
+
     end
-    return total_score
+    return score
 end
-puts calculate_score(moves_list)
+puts find_priority_score(rucksack_list)
 
 
