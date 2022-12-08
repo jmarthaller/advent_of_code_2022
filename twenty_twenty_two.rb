88fb1485f234342d7f2eded0ed2d7c66af005c48
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-08 09:15:34 -0800

    Clean files

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -300,22 +300,19 @@ rearrangements = File.readlines 'rearrangement_list.rb'
 # puts calculate_tree_cover(tree_patch)
 
 
-input = File.readlines('day_08_input.txt', chomp: true)
-            .map { |i| i.chars.map { |j| { height: j.to_i, visible: false, scores: [] } } }
+# input = File.readlines('day_08_input.txt', chomp: true)
+#             .map { |i| i.chars.map { |j| { height: j.to_i, visible: false, scores: [] } } }
 
-[input, input.transpose].each do |grid|
-  grid.each do |grid_row|
-    [grid_row, grid_row.reverse].each do |row|
-      row.reduce([]) do |memo, cell|
-        cell[:visible] = true if cell[:height] > (memo.max || -1)
-        cell[:scores].push((memo.reverse.index { |i| i >= cell[:height] } || (memo.size - 1)) + 1)
-        memo + [cell[:height]]
-      end
-    end
-  end
-end
-
-# puts input.flatten.select { |i| i[:visible].eql? true }.count
-puts input.flatten.map { |i| i[:scores].reduce(:*) }.max
-puts input.flatten.select { |i| i[:scores].reduce(:*) == 235200 }
+# [input, input.transpose].each do |grid|
+#   grid.each do |grid_row|
+#     [grid_row, grid_row.reverse].each do |row|
+#       row.reduce([]) do |memo, cell|
+#         cell[:visible] = true if cell[:height] > (memo.max || -1)
+#         cell[:scores].push((memo.reverse.index { |i| i >= cell[:height] } || (memo.size - 1)) + 1)
+#         memo + [cell[:height]]
+#       end
+#     end
+#   end
+# end
+# # puts input.flatten.select { |i| i[:visible].eql? true }.count
 # puts input.flatten.map { |i| i[:scores].reduce(:*) }.max
