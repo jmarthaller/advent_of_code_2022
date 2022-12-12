9a750e20f05411e3f08e552f1959943f3032b74b
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-12 08:59:56 -0800

    JS solve with reddit help Day 12

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -403,36 +403,156 @@ rearrangements = File.readlines 'rearrangement_list.rb'
 #     puts list
 # end
 # find_monkey_business(monkeys)
-[{ rounds: 20, div: 3 }, { rounds: 10_000, div: 1 }].each do |part|
-    monkeys = File.read('day_11_input.txt').split("\n\n").map do |i|
-      i = i.split("\n")
+# [{ rounds: 20, div: 3 }, { rounds: 10_000, div: 1 }].each do |part|
+#     monkeys = File.read('day_11_input.txt').split("\n\n").map do |i|
+#       i = i.split("\n")
   
-      {
-        items: i[1].scan(/\d+/).map(&:to_i),
-        oper: i[2].scan(/[+*]/).first.to_sym,
-        param: i[2].scan(/\d+$/).map(&:to_i).first,
-        test: i[3].scan(/\d+$/).first.to_i,
-        pass: i[4].scan(/\d+$/).first.to_i,
-        fail: i[5].scan(/\d+$/).first.to_i,
-        inspections: 0
-      }
-    end
+#       {
+#         items: i[1].scan(/\d+/).map(&:to_i),
+#         oper: i[2].scan(/[+*]/).first.to_sym,
+#         param: i[2].scan(/\d+$/).map(&:to_i).first,
+#         test: i[3].scan(/\d+$/).first.to_i,
+#         pass: i[4].scan(/\d+$/).first.to_i,
+#         fail: i[5].scan(/\d+$/).first.to_i,
+#         inspections: 0
+#       }
+#     end
   
-    lcm = monkeys.map { |i| i[:test] }.reduce(:lcm)
+#     lcm = monkeys.map { |i| i[:test] }.reduce(:lcm)
   
-    part[:rounds].times.each do
-      monkeys.each do |monkey|
-        monkey[:inspections] += monkey[:items].size
+#     part[:rounds].times.each do
+#       monkeys.each do |monkey|
+#         monkey[:inspections] += monkey[:items].size
   
-        while (i = monkey[:items].shift)
-          param = monkey[:param] || i
-          i = (i.method(monkey[:oper]).call(param) / part[:div]) % lcm
+#         while (i = monkey[:items].shift)
+#           param = monkey[:param] || i
+#           i = (i.method(monkey[:oper]).call(param) / part[:div]) % lcm
   
-          target = (i % monkey[:test]).zero? ? monkey[:pass] : monkey[:fail]
-          monkeys[target][:items] << i
+#           target = (i % monkey[:test]).zero? ? monkey[:pass] : monkey[:fail]
+#           monkeys[target][:items] << i
+#         end
+#       end
+#     end
+  
+#     puts monkeys.map { |i| i[:inspections] }.max(2).reduce(:*)
+#   end
+
+# day 12 graph search problem
+# path_heights = File.readlines('day_12_input.txt', chomp: true)
+
+# part one
+# def find_path(path)
+#     map = []
+#     File.readlines(path, chomp: true).each do |line|
+#         map <<
+#         line.chars.map do |x|
+#             if %w[S E].include?(x)
+#             if x == 'S'
+#                 0
+#             else
+#                 27
+#             end
+#             else
+#             x.ord - 96
+#             end
+#         end
+#     end
+#     start_x = 0
+#     start_y = 0
+#     end_x = 0
+#     end_y = 0
+#     map.each_with_index do |x, idx|
+#         x.each_with_index do |y, idy|
+#         if y == 27
+#             end_x = idx
+#             end_y = idy
+#         end
+#         if y == 0
+#             start_x = idx
+#             start_y = idy
+#         end
+#         end
+#     end
+#     visited = {}
+#     step(start_x, start_y, map, 0, visited)
+#     puts visited["#{end_x}:#{end_y}"]
+# end
+
+# def step(x, y, map, steps, visited)
+#     return visited if !visited["#{x}:#{y}"].nil? && steps >= visited["#{x}:#{y}"]
+
+#     visited["#{x}:#{y}"] = steps
+#     current = map[x][y]
+#     return visited if current == 27
+
+#     up = (y - 1 >= 0 ? map[x][y - 1] : 30)
+#     down = (y + 1 <= map.first.size - 1 ? map[x][y + 1] : 30)
+#     left = (x - 1 >= 0 ? map[x - 1][y] : 30)
+#     right = (x + 1 <= map.size - 1 ? map[x + 1][y] : 30)
+
+#     visited = step(x, y - 1, map, steps + 1, visited) if up - current < 2 || (current == 25 && up == 27)
+#     visited = step(x, y + 1, map, steps + 1, visited) if down - current < 2 || (current == 25 && down == 27)
+#     visited = step(x - 1, y, map, steps + 1, visited) if left - current < 2 || (current == 25 && left == 27)
+#     visited = step(x + 1, y, map, steps + 1, visited) if right - current < 2 || (current == 25 && right == 27)
+#     visited
+# end
+# find_path('day_12_input.txt')
+
+# part two
+def shortest_path_part_two(path)
+    map = []
+    File.readlines(path, chomp: true).each do |line|
+        map <<
+        line.chars.map do |x|
+            if %w[S E].include?(x)
+            if x == 'S'
+                0
+            else
+                27
+            end
+            else
+            x.ord - 96
+            end
         end
-      end
     end
-  
-    puts monkeys.map { |i| i[:inspections] }.max(2).reduce(:*)
-  end
+    end_x = 0
+    end_y = 0
+    points = []
+    map.each_with_index do |x, idx|
+        x.each_with_index do |y, idy|
+        if y == 27
+            end_x = idx
+            end_y = idy
+        end
+        points << [idx, idy] if y == 1
+        end
+    end
+    paths = []
+    points.each_with_index do |point, idx|
+        puts "#{idx}/#{points.size}"
+        visited = {}
+        step(point[0], point[1], map, 0, visited)
+        paths << visited["#{end_x}:#{end_y}"]
+    end
+    puts paths.compact.min
+end
+
+def step(x, y, map, steps, visited)
+    return visited if !visited["#{x}:#{y}"].nil? && steps >= visited["#{x}:#{y}"]
+
+    visited["#{x}:#{y}"] = steps
+    current = map[x][y]
+    return visited if current == 27
+
+    up = (y - 1 >= 0 ? map[x][y - 1] : 30)
+    down = (y + 1 <= map.first.size - 1 ? map[x][y + 1] : 30)
+    left = (x - 1 >= 0 ? map[x - 1][y] : 30)
+    right = (x + 1 <= map.size - 1 ? map[x + 1][y] : 30)
+
+    visited = step(x, y - 1, map, steps + 1, visited) if up - current < 2 || (current == 25 && up == 27)
+    visited = step(x, y + 1, map, steps + 1, visited) if down - current < 2 || (current == 25 && down == 27)
+    visited = step(x - 1, y, map, steps + 1, visited) if left - current < 2 || (current == 25 && left == 27)
+    visited = step(x + 1, y, map, steps + 1, visited) if right - current < 2 || (current == 25 && right == 27)
+    visited
+end
+shortest_path_part_two('day_12_input.txt')
