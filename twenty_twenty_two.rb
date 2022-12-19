506e94a4c2bbeff97dcd253a56ebb6aaa7fd3660
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-19 09:44:15 -0800

    Day 19 Ruby with reddit help

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -1106,99 +1106,233 @@ ROCKS = {
 
 
 # day 18
+# require "set"
+
+# class Cube
+#     attr_reader :x
+#     attr_reader :y
+#     attr_reader :z
+
+#     def initialize(x, y, z)
+#         @x = x
+#         @y = y
+#         @z = z
+#     end
+
+#     def neighbors
+#         [
+#             Cube.new(@x-1, @y, @z), Cube.new(@x+1, @y, @z),
+#             Cube.new(@x, @y-1, @z), Cube.new(@x, @y+1, @z),
+#             Cube.new(@x, @y, @z-1), Cube.new(@x, @y, @z+1)
+#         ]
+#     end
+
+#     def bounded_neighbors(x_bounds, y_bounds, z_bounds)
+#         neighbors.select { |c|
+#             x_bounds.include?(c.x) && y_bounds.include?(c.y) && z_bounds.include?(c.z) 
+#         }
+#     end
+
+#     def eql?(other)
+#         self.class == other.class && @x == other.x && @y == other.y && @z == other.z
+#     end
+
+#     def ==(other)
+#         self.eql?(other)
+#     end
+
+#     def hash
+#         [self.class, @x, @y, @z].hash
+#     end
+
+#     def to_s
+#         "(#{@x},#{@y},#{@z})"
+#     end
+# end
+
+# def parse_cube(str)
+#     Cube.new *(str.split(",").map { |s| s.to_i })
+# end
+
+# def surface_area(cubes)
+#     cubes.reduce(0) { |sum, cube| 
+#         adjacent = 6 - cube.neighbors.count { |n| cubes.include? n }
+#         sum + adjacent
+#     }
+# end
+
+# def bounding_ranges(cubes)
+#     first = cubes.first()
+#     min_x = max_x = first.x
+#     min_y = max_y = first.y
+#     min_z = max_z = first.z
+#     cubes.each do |c|
+#         min_x = c.x if c.x < min_x
+#         max_x = c.x if c.x > max_x
+#         min_y = c.y if c.y < min_y
+#         max_y = c.y if c.y > max_y
+#         min_z = c.z if c.z < min_z
+#         max_z = c.z if c.z > max_z
+#     end
+#     [((min_x - 1)..(max_x + 1)), ((min_y - 1)..(max_y + 1)), ((min_z - 1)..(max_z + 1))]
+# end
+
+# def external_surface_area(cubes)
+#     external_surfaces = 0
+#     (x_range, y_range, z_range) = bounding_ranges(cubes)
+
+#     open_cells = [Cube.new(x_range.first, y_range.first, z_range.first)]
+#     closed_cells = open_cells.to_set
+
+#     while open_cells.any?
+#         cell = open_cells.pop
+#         cell.bounded_neighbors(x_range, y_range, z_range).each do |neighbor|
+#             next if closed_cells.include? neighbor
+#             if cubes.include? neighbor
+#                 external_surfaces += 1
+#             else
+#                 closed_cells << neighbor
+#                 open_cells << neighbor
+#             end
+#         end
+#     end
+#     external_surfaces
+# end
+
+# cubes = File.read("day_18_input.txt").split.map { |line| parse_cube(line) }.to_set
+# puts "Part 1: #{surface_area cubes}"
+# puts "Part 2: #{external_surface_area cubes}"
+
+
+# day 19
+inputList = IO.readlines("day_19_input.txt").map(&:chomp)
+
 require "set"
 
-class Cube
-    attr_reader :x
-    attr_reader :y
-    attr_reader :z
+Blueprint = Struct.new(:id, :oreBotOre, :clayBotOre, 
+                       :obsBotOre, :obsBotClay, 
+                       :geodeBotOre, :geodeBotObs)
+blueprints = inputList.map{ |line| Blueprint.new(*line.scan(/(\d+)/).flatten.map(&:to_i)) }
 
-    def initialize(x, y, z)
-        @x = x
-        @y = y
-        @z = z
-    end
+BotState = Struct.new(:minutes, 
+   :oreBotCnt, :clayBotCnt, :obsBotCnt, :geodeBotCnt, 
+   :ore, :clay, :obs, :geode)
 
-    def neighbors
-        [
-            Cube.new(@x-1, @y, @z), Cube.new(@x+1, @y, @z),
-            Cube.new(@x, @y-1, @z), Cube.new(@x, @y+1, @z),
-            Cube.new(@x, @y, @z-1), Cube.new(@x, @y, @z+1)
-        ]
-    end
-
-    def bounded_neighbors(x_bounds, y_bounds, z_bounds)
-        neighbors.select { |c|
-            x_bounds.include?(c.x) && y_bounds.include?(c.y) && z_bounds.include?(c.z) 
-        }
-    end
-
-    def eql?(other)
-        self.class == other.class && @x == other.x && @y == other.y && @z == other.z
-    end
-
-    def ==(other)
-        self.eql?(other)
-    end
-
-    def hash
-        [self.class, @x, @y, @z].hash
-    end
-
-    def to_s
-        "(#{@x},#{@y},#{@z})"
-    end
-end
-
-def parse_cube(str)
-    Cube.new *(str.split(",").map { |s| s.to_i })
-end
-
-def surface_area(cubes)
-    cubes.reduce(0) { |sum, cube| 
-        adjacent = 6 - cube.neighbors.count { |n| cubes.include? n }
-        sum + adjacent
-    }
-end
-
-def bounding_ranges(cubes)
-    first = cubes.first()
-    min_x = max_x = first.x
-    min_y = max_y = first.y
-    min_z = max_z = first.z
-    cubes.each do |c|
-        min_x = c.x if c.x < min_x
-        max_x = c.x if c.x > max_x
-        min_y = c.y if c.y < min_y
-        max_y = c.y if c.y > max_y
-        min_z = c.z if c.z < min_z
-        max_z = c.z if c.z > max_z
-    end
-    [((min_x - 1)..(max_x + 1)), ((min_y - 1)..(max_y + 1)), ((min_z - 1)..(max_z + 1))]
-end
-
-def external_surface_area(cubes)
-    external_surfaces = 0
-    (x_range, y_range, z_range) = bounding_ranges(cubes)
-
-    open_cells = [Cube.new(x_range.first, y_range.first, z_range.first)]
-    closed_cells = open_cells.to_set
-
-    while open_cells.any?
-        cell = open_cells.pop
-        cell.bounded_neighbors(x_range, y_range, z_range).each do |neighbor|
-            next if closed_cells.include? neighbor
-            if cubes.include? neighbor
-                external_surfaces += 1
-            else
-                closed_cells << neighbor
-                open_cells << neighbor
+def getBestGeodes(bp, minutes)
+   best = 0
+   queue = [BotState.new(minutes, 1, 0, 0, 0, 0, 0, 0, 0)]
+   visited = Set.new
+   while(queue.size > 0)
+      curr = queue.shift
+      if(!visited.include?(curr))
+         visited << curr
+         if(curr.minutes==0)
+            if(best < curr.geode)
+               #puts "found new best #{bp.id}: #{curr.geode}"
+               best = curr.geode
             end
-        end
-    end
-    external_surfaces
+         # abandon thread if there is no way we could beat the current best
+         #elsif(curr.geode + ((1..curr.minutes).to_a.sum) > best)
+         #elsif(curr.minutes > 20 || curr.geode + ((1...curr.minutes).to_a.sum) > best)
+         else
+            n = BotState.new(
+               curr.minutes - 1,
+               curr.oreBotCnt,
+               curr.clayBotCnt,
+               curr.obsBotCnt,
+               curr.geodeBotCnt,
+               curr.ore + curr.oreBotCnt,
+               curr.clay + curr.clayBotCnt,
+               curr.obs + curr.obsBotCnt,
+               curr.geode + curr.geodeBotCnt
+            )
+           
+            # always build geode bot if you can 
+            canBuildGeodeBot = (curr.ore>=bp.geodeBotOre && curr.obs>=bp.geodeBotObs)
+            # don't build obs bot if you can build geode bot
+            canBuildObsBot = !canBuildGeodeBot && 
+               #don't need more obs than 1 geodeBot per min
+               (curr.obsBotCnt<bp.geodeBotObs) &&
+               (curr.ore>=bp.obsBotOre && curr.clay>=bp.obsBotClay)
+            # don't build clay bot if you can build geode bot
+            canBuildClayBot = !canBuildGeodeBot && !canBuildObsBot &&
+               #don't need more clay than 1 obsBot per min
+               (curr.clayBotCnt<bp.obsBotClay) && 
+               (curr.ore>=bp.clayBotOre)
+            canBuildOreBot = !canBuildGeodeBot && !canBuildObsBot &&
+               #don't need more ore than the max any bot could need per min
+               (curr.oreBotCnt<[bp.geodeBotOre,bp.obsBotOre,bp.clayBotOre].max) &&
+               (curr.ore>=bp.oreBotOre)
+            # don't want to hoard if we can build a a geode bot
+            canBuildNothing = !canBuildGeodeBot &&
+               # don't idle if we have already hoarded more clay and ore than we need
+               (curr.ore < 2*[bp.geodeBotOre,bp.obsBotOre,bp.clayBotOre].max) &&
+               (curr.clay < 3*bp.obsBotClay)
+
+            # push build nothing state only 
+            queue << n if(canBuildNothing)
+
+               # push build geode bot state
+            queue << BotState.new(
+               n.minutes,
+               n.oreBotCnt, 
+               n.clayBotCnt,
+               n.obsBotCnt, 
+               n.geodeBotCnt + 1,
+               n.ore - bp.geodeBotOre, 
+               n.clay,
+               n.obs - bp.geodeBotObs,
+               n.geode
+            ) if(canBuildGeodeBot)
+
+            # push build obs bot state
+            queue << BotState.new(
+               n.minutes,
+               n.oreBotCnt, 
+               n.clayBotCnt,
+               n.obsBotCnt + 1, 
+               n.geodeBotCnt,
+               n.ore - bp.obsBotOre,
+               n.clay - bp.obsBotClay,
+               n.obs,
+               n.geode
+            ) if(canBuildObsBot)
+            
+            # push build clay bot state
+            queue << BotState.new(
+               n.minutes,
+               n.oreBotCnt, 
+               n.clayBotCnt + 1,
+               n.obsBotCnt, 
+               n.geodeBotCnt,
+               n.ore - bp.clayBotOre,
+               n.clay,
+               n.obs,
+               n.geode
+            ) if(canBuildClayBot)
+            
+            # push build ore bot state
+            queue << BotState.new(
+               n.minutes,
+               n.oreBotCnt + 1, 
+               n.clayBotCnt,
+               n.obsBotCnt, 
+               n.geodeBotCnt,
+               n.ore - bp.oreBotOre,
+               n.clay,
+               n.obs,
+               n.geode
+            ) if(canBuildOreBot)
+         end
+      end
+   end
+   return best
 end
 
-cubes = File.read("day_18_input.txt").split.map { |line| parse_cube(line) }.to_set
-puts "Part 1: #{surface_area cubes}"
-puts "Part 2: #{external_surface_area cubes}"
+puts blueprints.map { |bp|
+   getBestGeodes(bp, 24) * bp.id
+}.sum
+
+puts blueprints[0..2].map { |bp|
+   getBestGeodes(bp, 32)
+}.inject(1,:*)
