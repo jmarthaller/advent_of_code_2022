57fdbe55b32b7871880b2d63ca2035953994dffc
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-21 06:21:47 -0800

    Day 21

--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -1338,8 +1338,95 @@ ROCKS = {
 # }.inject(1,:*)
 
 # day 20
-[1, 811589153].each{|n|
-    a = File.readlines('day_20_input.txt').each_with_index.map{[_1.to_i * n, _2]}
-    (n % 71).times{a.size.times{|i| a.insert(
-      ((j = a.index{_1[1] == i}) + a[j][0]) % (a.size - 1), a.delete_at(j))}}
-    p [1, 2, 3].map{|x| a[(a.index{_1[0] == 0} + x * 1000) % a.size]}.sum{_1[0]}}
+# [1, 811589153].each{|n|
+#     a = File.readlines('day_20_input.txt').each_with_index.map{[_1.to_i * n, _2]}
+#     (n % 71).times{a.size.times{|i| a.insert(
+#       ((j = a.index{_1[1] == i}) + a[j][0]) % (a.size - 1), a.delete_at(j))}}
+#     p [1, 2, 3].map{|x| a[(a.index{_1[0] == 0} + x * 1000) % a.size]}.sum{_1[0]}}
+
+# day 21
+INPUT = "day_21_input.txt"
+  ROOT = "root"
+  HUMAN = "humn"
+
+
+    @monkeys = {}
+    File.readlines(INPUT, chomp: true).each do |line|
+      key, val = line.split(": ")
+      if val =~ /(\w*) ([\+\*\/\-]) (\w*)/
+        @monkeys[key] = [$2, $1, $3]
+      else
+        @monkeys[key] = val.to_i
+      end
+    end
+
+
+  def one
+    solve(ROOT)
+  end
+
+  def two
+    left_path = path_to_human(@monkeys[ROOT][1])
+    right_path = path_to_human(@monkeys[ROOT][2])
+    if left_path.is_a?(Integer)
+      solve_equation(right_path, left_path)
+    else
+      solve_equation(left_path, right_path)
+    end
+  end
+
+  private def solve(key)
+    return @monkeys[key] if @monkeys[key].is_a?(Integer)
+
+    op, left, right = @monkeys[key]
+    left_val = solve(left)
+    right_val = solve(right)
+    left_val.send(op, right_val)
+  end
+
+  private def solve_equation(equation, value)
+    return value if equation == HUMAN
+    op, left, right = equation
+    if op == "+"
+      if left.is_a?(Integer)
+        solve_equation(right, value - left)
+      else
+        solve_equation(left, value - right)
+      end
+    elsif op == "-"
+      if left.is_a?(Integer)
+        solve_equation(right, left - value)
+      else
+        solve_equation(left, right + value)
+      end
+    elsif op == "/"
+      if left.is_a?(Integer)
+        solve_equation(right, left / value)
+      else
+        solve_equation(left, right * value)
+      end
+    else
+      if left.is_a?(Integer)
+        solve_equation(right, value / left)
+      else
+        solve_equation(left, value / right)
+      end
+    end
+  end
+
+  private def path_to_human(key)
+    return HUMAN if key == HUMAN
+    return @monkeys[key] if @monkeys[key].is_a?(Integer)
+
+    op, left, right = @monkeys[key]
+    left_val = path_to_human(left)
+    right_val = path_to_human(right)
+    if left_val.is_a?(Integer) && right_val.is_a?(Integer)
+      left_val.send(op, right_val)
+    else
+      [op, left_val, right_val]
+    end
+  end
+
+  puts one()
+  puts two()
