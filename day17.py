00e7dbcea715702218a753206171dedcb5c8cffe
Jacob Marthaller <marthaller.jr@gmail.com>
2022-12-22 16:35:40 -0800

    Clean up day 22

--- day17.py
+++ day17.py
@@ -143,226 +143,276 @@
 #     print("elapsed time: %fms\n" % ((timeit.default_timer()-starttime)*1000))
 
 
-from __future__ import annotations
+# from __future__ import annotations
  
-from itertools import chain
-from itertools import cycle
-from itertools import repeat
- 
- 
-def parse_input(filepath) -> str:
-    with open(filepath, "r") as f:
-        return f.read().strip()
+# from itertools import chain
+# from itertools import cycle
+# from itertools import repeat
  
  
-class Rock:
-    def __init__(self):
-        self.coordinates = set()
- 
-    def __str__(self):
-        return f"{self.__class__.__name__}({self.coordinates})"
- 
-    def __repr__(self):
-        return str(self)
- 
-    def max_height(self):
-        max_height = 0
-        for coordinate in self.coordinates:
-            max_height = max(max_height, coordinate.imag)
-        return max_height
- 
-    def move_left(self) -> Rock:
-        rock = Rock()
-        for coordinate in self.coordinates:
-            rock.coordinates.add(coordinate + complex(-1, 0))
-        return rock
- 
-    def move_right(self) -> Rock:
-        rock = Rock()
-        for coordinate in self.coordinates:
-            rock.coordinates.add(coordinate + complex(1, 0))
-        return rock
- 
-    def move_down(self) -> Rock:
-        rock = Rock()
-        for coordinate in self.coordinates:
-            rock.coordinates.add(coordinate + complex(0, -1))
-        return rock
- 
-    @classmethod
-    def flat(cls, start_x, start_y) -> Rock:
-        rock = Rock()
-        initial_configuration = [
-            complex(0, 0),
-            complex(1, 0),
-            complex(2, 0),
-            complex(3, 0),
-        ]
-        for position in initial_configuration:
-            position += complex(start_x, start_y)
-            rock.coordinates.add(position)
-        return rock
- 
-    @classmethod
-    def cross(cls, start_x, start_y):
-        rock = Rock()
-        initial_configuration = [
-            complex(1, 0),
-            complex(0, 1),
-            complex(1, 1),
-            complex(2, 1),
-            complex(1, 2),
-        ]
-        for position in initial_configuration:
-            position += complex(start_x, start_y)
-            rock.coordinates.add(position)
-        return rock
- 
-    @classmethod
-    def corner(cls, start_x, start_y):
-        rock = Rock()
-        initial_configuration = [
-            complex(0, 0),
-            complex(1, 0),
-            complex(2, 0),
-            complex(2, 1),
-            complex(2, 2),
-        ]
-        for position in initial_configuration:
-            position += complex(start_x, start_y)
-            rock.coordinates.add(position)
-        return rock
- 
-    @classmethod
-    def tall(cls, start_x, start_y):
-        rock = Rock()
-        initial_configuration = [
-            complex(0, 0),
-            complex(0, 1),
-            complex(0, 2),
-            complex(0, 3),
-        ]
-        for position in initial_configuration:
-            position += complex(start_x, start_y)
-            rock.coordinates.add(position)
-        return rock
- 
-    @classmethod
-    def square_rock(cls, start_x, start_y):
-        rock = Rock()
-        initial_configuration = [
-            complex(0, 0),
-            complex(1, 0),
-            complex(0, 1),
-            complex(1, 1),
-        ]
-        for position in initial_configuration:
-            position += complex(start_x, start_y)
-            rock.coordinates.add(position)
-        return rock
+# def parse_input(filepath) -> str:
+#     with open(filepath, "r") as f:
+#         return f.read().strip()
  
  
-class Cave:
-    rock_shapes = (Rock.flat, Rock.cross, Rock.corner, Rock.tall, Rock.square_rock)
+# class Rock:
+#     def __init__(self):
+#         self.coordinates = set()
  
-    def __init__(self, width: int, moves: str):
-        self.moves = moves  # <><><<<>><>
-        self.width = width
-        self.rock_shapes_index = 0
-        self.height = 1
-        self._drop_height = 3
-        self.start_x = 2
-        self.rock_positions = set()
-        self.jet_generator = chain.from_iterable(zip(cycle(self.moves), repeat(None)))
+#     def __str__(self):
+#         return f"{self.__class__.__name__}({self.coordinates})"
  
-    @property
-    def start_y(self):
-        return self.height + self._drop_height
+#     def __repr__(self):
+#         return str(self)
  
-    def get_next_rock(self) -> Rock:
-        r = self.rock_shapes[self.rock_shapes_index](self.start_x, self.start_y)
-        self.update_rock_shape_index()
-        return r
+#     def max_height(self):
+#         max_height = 0
+#         for coordinate in self.coordinates:
+#             max_height = max(max_height, coordinate.imag)
+#         return max_height
  
-    def update_rock_shape_index(self):
-        self.rock_shapes_index = (self.rock_shapes_index + 1) % len(self.rock_shapes)
+#     def move_left(self) -> Rock:
+#         rock = Rock()
+#         for coordinate in self.coordinates:
+#             rock.coordinates.add(coordinate + complex(-1, 0))
+#         return rock
  
-    def clear_of_collision_check(self, rock: Rock) -> bool:
-        if self.rock_positions & rock.coordinates:
-            return False
-        else:
-            for coordinate in rock.coordinates:
-                if (coordinate.real == -1) or (coordinate.real == self.width):
-                    return False
-                if coordinate.imag == 0:
-                    return False
-        return True
+#     def move_right(self) -> Rock:
+#         rock = Rock()
+#         for coordinate in self.coordinates:
+#             rock.coordinates.add(coordinate + complex(1, 0))
+#         return rock
  
-    def drop_rock(self):
-        rock = self.get_next_rock()
+#     def move_down(self) -> Rock:
+#         rock = Rock()
+#         for coordinate in self.coordinates:
+#             rock.coordinates.add(coordinate + complex(0, -1))
+#         return rock
  
-        for direction in self.jet_generator:
-            match direction:
-                case "<":
-                    next_position = rock.move_left()
-                    if self.clear_of_collision_check(next_position):
-                        rock = next_position
-                case ">":
-                    next_position = rock.move_right()
-                    if self.clear_of_collision_check(next_position):
-                        rock = next_position
-                case _:
-                    next_position = rock.move_down()
-                    if self.clear_of_collision_check(next_position):
-                        rock = next_position
-                    else:
-                        self.rock_positions.update(rock.coordinates)
-                        height_diff = rock.max_height() + 1 - self.height
-                        self.height = max(self.height, rock.max_height() + 1)
-                        return 0 if height_diff < 1 else height_diff
+#     @classmethod
+#     def flat(cls, start_x, start_y) -> Rock:
+#         rock = Rock()
+#         initial_configuration = [
+#             complex(0, 0),
+#             complex(1, 0),
+#             complex(2, 0),
+#             complex(3, 0),
+#         ]
+#         for position in initial_configuration:
+#             position += complex(start_x, start_y)
+#             rock.coordinates.add(position)
+#         return rock
+ 
+#     @classmethod
+#     def cross(cls, start_x, start_y):
+#         rock = Rock()
+#         initial_configuration = [
+#             complex(1, 0),
+#             complex(0, 1),
+#             complex(1, 1),
+#             complex(2, 1),
+#             complex(1, 2),
+#         ]
+#         for position in initial_configuration:
+#             position += complex(start_x, start_y)
+#             rock.coordinates.add(position)
+#         return rock
+ 
+#     @classmethod
+#     def corner(cls, start_x, start_y):
+#         rock = Rock()
+#         initial_configuration = [
+#             complex(0, 0),
+#             complex(1, 0),
+#             complex(2, 0),
+#             complex(2, 1),
+#             complex(2, 2),
+#         ]
+#         for position in initial_configuration:
+#             position += complex(start_x, start_y)
+#             rock.coordinates.add(position)
+#         return rock
+ 
+#     @classmethod
+#     def tall(cls, start_x, start_y):
+#         rock = Rock()
+#         initial_configuration = [
+#             complex(0, 0),
+#             complex(0, 1),
+#             complex(0, 2),
+#             complex(0, 3),
+#         ]
+#         for position in initial_configuration:
+#             position += complex(start_x, start_y)
+#             rock.coordinates.add(position)
+#         return rock
+ 
+#     @classmethod
+#     def square_rock(cls, start_x, start_y):
+#         rock = Rock()
+#         initial_configuration = [
+#             complex(0, 0),
+#             complex(1, 0),
+#             complex(0, 1),
+#             complex(1, 1),
+#         ]
+#         for position in initial_configuration:
+#             position += complex(start_x, start_y)
+#             rock.coordinates.add(position)
+#         return rock
  
  
-def find_pattern(heights: list[int]):
-    max_pattern_length = len(heights) // 2
-    for pattern_length in range(2, max_pattern_length):
-        for i in range(0, len(heights) - pattern_length):
-            window = heights[i : i + pattern_length]
-            duplicates = 0
-            next_start = i + pattern_length
-            next_stop = next_start + pattern_length
-            while window == heights[next_start:next_stop]:
-                duplicates += 1
-                if duplicates >= 3:
-                    return i, window
-                next_start, next_stop = (
-                    next_start + pattern_length,
-                    next_stop + pattern_length,
-                )
+# class Cave:
+#     rock_shapes = (Rock.flat, Rock.cross, Rock.corner, Rock.tall, Rock.square_rock)
+ 
+#     def __init__(self, width: int, moves: str):
+#         self.moves = moves  # <><><<<>><>
+#         self.width = width
+#         self.rock_shapes_index = 0
+#         self.height = 1
+#         self._drop_height = 3
+#         self.start_x = 2
+#         self.rock_positions = set()
+#         self.jet_generator = chain.from_iterable(zip(cycle(self.moves), repeat(None)))
+ 
+#     @property
+#     def start_y(self):
+#         return self.height + self._drop_height
+ 
+#     def get_next_rock(self) -> Rock:
+#         r = self.rock_shapes[self.rock_shapes_index](self.start_x, self.start_y)
+#         self.update_rock_shape_index()
+#         return r
+ 
+#     def update_rock_shape_index(self):
+#         self.rock_shapes_index = (self.rock_shapes_index + 1) % len(self.rock_shapes)
+ 
+#     def clear_of_collision_check(self, rock: Rock) -> bool:
+#         if self.rock_positions & rock.coordinates:
+#             return False
+#         else:
+#             for coordinate in rock.coordinates:
+#                 if (coordinate.real == -1) or (coordinate.real == self.width):
+#                     return False
+#                 if coordinate.imag == 0:
+#                     return False
+#         return True
+ 
+#     def drop_rock(self):
+#         rock = self.get_next_rock()
+ 
+#         for direction in self.jet_generator:
+#             match direction:
+#                 case "<":
+#                     next_position = rock.move_left()
+#                     if self.clear_of_collision_check(next_position):
+#                         rock = next_position
+#                 case ">":
+#                     next_position = rock.move_right()
+#                     if self.clear_of_collision_check(next_position):
+#                         rock = next_position
+#                 case _:
+#                     next_position = rock.move_down()
+#                     if self.clear_of_collision_check(next_position):
+#                         rock = next_position
+#                     else:
+#                         self.rock_positions.update(rock.coordinates)
+#                         height_diff = rock.max_height() + 1 - self.height
+#                         self.height = max(self.height, rock.max_height() + 1)
+#                         return 0 if height_diff < 1 else height_diff
  
  
-def part_one(filepath) -> int:
-    moves = parse_input(filepath)
-    cave = Cave(width=7, moves=moves)
-    for i in range(2022):
-        cave.drop_rock()
-    return int(cave.height - 1)
+# def find_pattern(heights: list[int]):
+#     max_pattern_length = len(heights) // 2
+#     for pattern_length in range(2, max_pattern_length):
+#         for i in range(0, len(heights) - pattern_length):
+#             window = heights[i : i + pattern_length]
+#             duplicates = 0
+#             next_start = i + pattern_length
+#             next_stop = next_start + pattern_length
+#             while window == heights[next_start:next_stop]:
+#                 duplicates += 1
+#                 if duplicates >= 3:
+#                     return i, window
+#                 next_start, next_stop = (
+#                     next_start + pattern_length,
+#                     next_stop + pattern_length,
+#                 )
  
  
-def part_two(filepath) -> int:
-    moves = parse_input(filepath)
-    height_changes = []
-    cave = Cave(width=7, moves=moves)
-    for i in range(20000):
-        height_changes.append(int(cave.drop_rock()))
+# def part_one(filepath) -> int:
+#     moves = parse_input(filepath)
+#     cave = Cave(width=7, moves=moves)
+#     for i in range(2022):
+#         cave.drop_rock()
+#     return int(cave.height - 1)
  
-    start, window = find_pattern(height_changes)
-    print(f"start: {start}\twindow length: {len(window)}")
-    left = sum(height_changes[:start])
-    middle, right = divmod(1000000000000 - start, len(window))
-    middle = middle * sum(window)
-    right = sum(window[:right])
  
-    return int(left + middle + right)
+# def part_two(filepath) -> int:
+#     moves = parse_input(filepath)
+#     height_changes = []
+#     cave = Cave(width=7, moves=moves)
+#     for i in range(20000):
+#         height_changes.append(int(cave.drop_rock()))
+ 
+#     start, window = find_pattern(height_changes)
+#     print(f"start: {start}\twindow length: {len(window)}")
+#     left = sum(height_changes[:start])
+#     middle, right = divmod(1000000000000 - start, len(window))
+#     middle = middle * sum(window)
+#     right = sum(window[:right])
+ 
+#     return int(left + middle + right)
 
 
-print(part_two('input_day_17.txt'))
+# print(part_two('input_day_17.txt'))
+
+
+directions = {
+    0 : lambda g, d, s, j, x, y : (d, z) if (z := (x + 1, y)) in g else (j[divmod(z[0], s), z[1] // s](s, x, y) if j else (d, min(g, key=lambda a: (abs(a[1] - y), a[0])))),
+    1 : lambda g, d, s, j, x, y : (d, z) if (z := (x, y + 1)) in g else (j[z[0] // s, divmod(z[1], s)](s, x, y) if j else (d, min(g, key=lambda a: (abs(a[0] - x), a[1])))),
+    2 : lambda g, d, s, j, x, y : (d, z) if (z := (x - 1, y)) in g else (j[divmod(z[0], s), z[1] // s](s, x, y) if j else (d, min(g, key=lambda a: (abs(a[1] - y), -a[0])))),
+    3 : lambda g, d, s, j, x, y : (d, z) if (z := (x, y - 1)) in g else (j[z[0] // s, divmod(z[1], s)](s, x, y) if j else (d, min(g, key=lambda a: (abs(a[0] - x), -a[1]))))
+}
+
+get_jumps = lambda size : {
+    ((3, 0), 0) :           lambda s, x, y : (2, (2 * s - 1, (3 * s) - 1 - y)),             #A to H  
+    (2, (-1, size - 1)) :   lambda s, x, y : (3, (x % s, (4 * s) - 1)),                     #D to M            ------ ------
+    (2, (1, 0)) :           lambda s, x, y : (2, ((s * 2) - 1, s + (x % s))),               #E to F           |   B  |  D   |
+    (1, (-1, size - 1)) :   lambda s, x, y : (0, (0, (s * 3) + (x % s))),                   #B to N           |C     |  E  A|
+    ((0, size - 1), 0) :    lambda s, x, y : (0, (0, (s * 3) - 1 - y)),                     #C to K            ------ ------
+    ((0, size - 1), 1) :    lambda s, x, y : (1, ((y % s), s * 2)),                         #G to J           |     F|
+    ((2, 0), 1) :           lambda s, x, y : (3, ((s * 2) + (y % s), s - 1)),               #F to E           |G     |
+    (0, (1, size - 1)) :    lambda s, x, y : (0, (s, s + x)),                               #J to G     ------ ------
+    ((-1, size - 1), 2) :   lambda s, x, y : (0, (s, s - 1 - (y % s))),                     #K to C    |   J  |     H|
+    ((-1, size - 1), 3) :   lambda s, x, y : (1, (s + y % s, 0)),                           #N to B    |K     |  I   |
+    (0, (4, 0)) :           lambda s, x, y : (1, (s * 2 + x, 0)),                           #M to D     ------ ------   
+    ((1, 0), 3) :           lambda s, x, y : (3, (s + (y % s), s * 3 - 1)),                 #L to I    |N    L|
+    (1, (3, 0)) :           lambda s, x, y : (2, (s - 1, s * 3 + (x % s))),                 #I to L    |   M  |
+    ((2, 0), 2) :           lambda s, x, y : (2, (s * 3 - 1, s - 1 - (y % s)))              #H to A     ------
+}
+
+def walk(instructions, grid, direction, size, current, jump=None):
+    while instructions:
+        instruction = instructions[:(i := next((e for e, x in enumerate(instructions) if x.isalpha()), len(instructions))) + 1]
+        instructions = instructions[i + 1:]
+        steps, turn = '', ''
+        for x in instruction:
+            steps, turn = (steps + x, turn) if x.isdigit() else (steps, x)
+        if steps:
+            for _ in range(int(steps)):
+                if grid[(nxt := directions[direction](grid, direction, size, jump, *current))[1]] != "#":
+                    direction, current = nxt
+                else:
+                    break
+        if turn:
+            direction = (direction + (1 if turn == "R" else -1)) % 4
+    return 1000 * (current[1] + 1) + 4 * (current[0] + 1) + next((e for e, x in enumerate(directions) if direction == x))
+
+with open("day_22_input.txt", "r") as file:
+    data, instructions = file.read().split("\n\n")
+    data = data.splitlines()
+    size = min(len(x.replace(" ", "")) for x in data)
+    grid = {(x, y) : data[y][x] for x in range(max(len(x) for x in data)) for y in range(len(data)) if x < len(data[y]) and data[y][x] != " "}
+    current = min(grid, key=lambda x: (x[1], x[0]))
+    print("day 22: ", walk(instructions[:], grid, 0, size, current), walk(instructions[:], grid, 0, size, current, get_jumps(size)))
--- twenty_twenty_two.rb
+++ twenty_twenty_two.rb
@@ -1345,88 +1345,242 @@ ROCKS = {
 #     p [1, 2, 3].map{|x| a[(a.index{_1[0] == 0} + x * 1000) % a.size]}.sum{_1[0]}}
 
 # day 21
-INPUT = "day_21_input.txt"
-  ROOT = "root"
-  HUMAN = "humn"
+# INPUT = "day_21_input.txt"
+#   ROOT = "root"
+#   HUMAN = "humn"
 
 
-    @monkeys = {}
-    File.readlines(INPUT, chomp: true).each do |line|
-      key, val = line.split(": ")
-      if val =~ /(\w*) ([\+\*\/\-]) (\w*)/
-        @monkeys[key] = [$2, $1, $3]
+#     @monkeys = {}
+#     File.readlines(INPUT, chomp: true).each do |line|
+#       key, val = line.split(": ")
+#       if val =~ /(\w*) ([\+\*\/\-]) (\w*)/
+#         @monkeys[key] = [$2, $1, $3]
+#       else
+#         @monkeys[key] = val.to_i
+#       end
+#     end
+
+
+#   def one
+#     solve(ROOT)
+#   end
+
+#   def two
+#     left_path = path_to_human(@monkeys[ROOT][1])
+#     right_path = path_to_human(@monkeys[ROOT][2])
+#     if left_path.is_a?(Integer)
+#       solve_equation(right_path, left_path)
+#     else
+#       solve_equation(left_path, right_path)
+#     end
+#   end
+
+#   private def solve(key)
+#     return @monkeys[key] if @monkeys[key].is_a?(Integer)
+
+#     op, left, right = @monkeys[key]
+#     left_val = solve(left)
+#     right_val = solve(right)
+#     left_val.send(op, right_val)
+#   end
+
+#   private def solve_equation(equation, value)
+#     return value if equation == HUMAN
+#     op, left, right = equation
+#     if op == "+"
+#       if left.is_a?(Integer)
+#         solve_equation(right, value - left)
+#       else
+#         solve_equation(left, value - right)
+#       end
+#     elsif op == "-"
+#       if left.is_a?(Integer)
+#         solve_equation(right, left - value)
+#       else
+#         solve_equation(left, right + value)
+#       end
+#     elsif op == "/"
+#       if left.is_a?(Integer)
+#         solve_equation(right, left / value)
+#       else
+#         solve_equation(left, right * value)
+#       end
+#     else
+#       if left.is_a?(Integer)
+#         solve_equation(right, value / left)
+#       else
+#         solve_equation(left, value / right)
+#       end
+#     end
+#   end
+
+#   private def path_to_human(key)
+#     return HUMAN if key == HUMAN
+#     return @monkeys[key] if @monkeys[key].is_a?(Integer)
+
+#     op, left, right = @monkeys[key]
+#     left_val = path_to_human(left)
+#     right_val = path_to_human(right)
+#     if left_val.is_a?(Integer) && right_val.is_a?(Integer)
+#       left_val.send(op, right_val)
+#     else
+#       [op, left_val, right_val]
+#     end
+#   end
+
+#   puts one()
+#   puts two()
+
+
+# day 23
+map = ['']
+instructions = []
+File.foreach('day_22_input.txt') do |line|
+  md = line.match(/\d/)
+  if md
+    line.scan(/(\d+|R|L)/) do |m|
+      if 'LR'.include?(m[0])
+        instructions << m[0].to_sym
       else
-        @monkeys[key] = val.to_i
+        instructions << m[0].to_i
       end
     end
+  else
+    map << ' ' + line.chomp + ' '
+  end
+end
+map << ''
+width = map.map(&:size).max
+map = map.map { |line| line.ljust(width) }
+$map = map
 
+def transform(in_a, in_b, out_a, out_b, input)
+  mult = (out_b - out_a) / (in_b - in_a)
+  raise if ![-1, 1].include?(mult)
+  out_a + (input - in_a) * mult
+end
 
-  def one
-    solve(ROOT)
+def move1(x, y, face, sanity=false)
+  new_x = x
+  new_y = y
+  new_face = face
+
+  while true
+    tmp_x, tmp_y, tmp_face = new_x, new_y, new_face
+
+    dx = [1, 0, -1, 0].fetch(new_face)
+    dy = [0, 1, 0, -1].fetch(new_face)
+    new_y = (new_y + dy) % $map.size
+    new_x = (new_x + dx) % $map[new_y].size
+
+    if $map[new_y][new_x] == ' '
+      if new_x == 151 && (1..50).include?(new_y)       # B to D
+        new_x, new_y = 100, transform(1, 50, 150, 101, new_y)
+        new_face = 2
+
+      elsif new_x == 101 && (101..150).include?(new_y) # D to B
+        new_x, new_y = 150, transform(150, 101, 1, 50, new_y)
+        new_face = 2
+
+      elsif (51..100).include?(new_x) && new_y == 0    # A to F
+        new_x, new_y = 1, transform(51, 100, 151, 200, new_x)
+        new_face = 0
+
+      elsif new_x == 0 && (151..200).include?(new_y)   # F to A
+        new_x, new_y = transform(151, 200, 51, 100, new_y), 1
+        new_face = 1
+
+      elsif (1..50).include?(new_x) && new_y == 201    # F to B
+        new_x, new_y = new_x + 100, 1
+        new_face = 1  # redundant
+
+      elsif (101..150).include?(new_x) && new_y == 0   # B to F
+        new_x, new_y = new_x - 100, 200
+        new_face = 3  # redundant
+
+      elsif new_x == 51 && (151..200).include?(new_y)  # F to D
+        new_x, new_y = transform(151, 200, 51, 100, new_y), 150
+        new_face = 3
+
+      elsif (51..100).include?(new_x) && new_y == 151  # D to F
+        new_x, new_y = 50, transform(51, 100, 151, 200, new_x)
+        new_face = 2
+
+      elsif new_x == 50 && (1..50).include?(new_y)     # A to E
+        new_x, new_y = 1, transform(1, 50, 150, 101, new_y)
+        new_face = 0
+
+      elsif new_x == 0 && (101..150).include?(new_y)   # E to A
+        new_x, new_y = 51, transform(150, 101, 1, 50, new_y)
+        new_face = 0
+
+      elsif (1..50).include?(new_x) && new_y == 100    # E to C
+        new_x, new_y = 51, transform(1, 50, 51, 100, new_x)
+        new_face = 0
+
+      elsif new_x == 50 && (51..100).include?(new_y)   # C to E
+        new_x, new_y = transform(51, 100, 1, 50, new_y), 101
+        new_face = 1
+
+      elsif new_x == 101 && (51..100).include?(new_y)  # C to B
+        new_x, new_y = transform(51, 100, 101, 150, new_y), 50
+        new_face = 3
+
+      elsif (101..150).include?(new_x) && new_y == 51  # B to C
+        new_x, new_y = 100, transform(101, 150, 51, 100, new_x)
+        new_face = 2
+
+      else
+        $stderr.puts "Unknown space: #{new_x},#{new_y}, #{new_face}"
+        exit 2
+      end
+
+      # Sanity check: if we moved backwards do we get to where we were before?
+      if !sanity
+        s_x, s_y, s_face = move1(new_x, new_y, ((new_face + 2) % 4), true)
+        if s_x != tmp_x || s_y != tmp_y || s_face != ((tmp_face + 2) % 4)
+          $stderr.puts "Sanity check failed."
+          $stderr.puts "tmp:  #{tmp_x},#{tmp_y}, #{tmp_face}"
+          $stderr.puts "new:  #{new_x},#{new_y}, #{new_face}"
+          $stderr.puts "sane: #{s_x},#{s_y}, #{s_face}"
+          exit 3
+        end
+      end
+    end
+    if $map[new_y][new_x] == ' '
+      $stderr.puts "Still in a space: #{new_x},#{new_y}, #{new_face}"
+      exit 3
+    end
+    break
   end
 
-  def two
-    left_path = path_to_human(@monkeys[ROOT][1])
-    right_path = path_to_human(@monkeys[ROOT][2])
-    if left_path.is_a?(Integer)
-      solve_equation(right_path, left_path)
-    else
-      solve_equation(left_path, right_path)
+  [new_x, new_y, new_face]
+end
+
+y = 1
+x = map[y].index('.')
+face = 0   # 0 = right, 1 = down, etc.
+
+instructions.each do |inst|
+  puts "Executing #{inst}"
+  case inst
+  when :L
+    face = (face - 1) % 4
+  when :R
+    face = (face + 1) % 4
+  when Integer
+    inst.times do
+      new_x, new_y, new_face = move1(x, y, face)
+      if map[new_y][new_x] == '.'
+        x = new_x
+        y = new_y
+        face = new_face
+      elsif map[new_y][new_x] == '#'
+        break  # Blocked by a wall.
+      end
     end
   end
+end
+map[y][x] = '*'
 
-  private def solve(key)
-    return @monkeys[key] if @monkeys[key].is_a?(Integer)
-
-    op, left, right = @monkeys[key]
-    left_val = solve(left)
-    right_val = solve(right)
-    left_val.send(op, right_val)
-  end
-
-  private def solve_equation(equation, value)
-    return value if equation == HUMAN
-    op, left, right = equation
-    if op == "+"
-      if left.is_a?(Integer)
-        solve_equation(right, value - left)
-      else
-        solve_equation(left, value - right)
-      end
-    elsif op == "-"
-      if left.is_a?(Integer)
-        solve_equation(right, left - value)
-      else
-        solve_equation(left, right + value)
-      end
-    elsif op == "/"
-      if left.is_a?(Integer)
-        solve_equation(right, left / value)
-      else
-        solve_equation(left, right * value)
-      end
-    else
-      if left.is_a?(Integer)
-        solve_equation(right, value / left)
-      else
-        solve_equation(left, value / right)
-      end
-    end
-  end
-
-  private def path_to_human(key)
-    return HUMAN if key == HUMAN
-    return @monkeys[key] if @monkeys[key].is_a?(Integer)
-
-    op, left, right = @monkeys[key]
-    left_val = path_to_human(left)
-    right_val = path_to_human(right)
-    if left_val.is_a?(Integer) && right_val.is_a?(Integer)
-      left_val.send(op, right_val)
-    else
-      [op, left_val, right_val]
-    end
-  end
-
-  puts one()
-  puts two()
+puts 1000 * y + 4 * x + face
